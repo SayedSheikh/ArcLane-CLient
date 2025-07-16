a1c6ad528fef102c41b252886a3c4022ed79e1a6
Sayed Sheikh <sayedsheikh9@gmail.com>
2025-07-16 14:19:59 +0600

    route change spinner added

--- src/Layouts/MainLayout/MainLayout.jsx
+++ src/Layouts/MainLayout/MainLayout.jsx
@@ -1,15 +1,20 @@
 import React from "react";
-import { Outlet } from "react-router";
+import { Outlet, useNavigation } from "react-router";
 
 import CustomeNavbar from "../../Pages/Shared/Navbar/CustomeNavbar";
 import CustomFooter from "../../Pages/Shared/Footer/CustomFooter";
+import ScrollToTop from "../../Pages/Shared/ScrollToTop/ScrollToTop";
+import Loading1 from "../../Pages/Loadings/Loading1";
 
 const MainLayout = () => {
+  const navigation = useNavigation();
+
   return (
     <div className="font-inter">
+      <ScrollToTop />
       <CustomeNavbar />
-      <div className="min-h-[calc(100vh-60px)] dark:dark:bg-[#030712]">
-        <Outlet />
+      <div className="min-h-[calc(100vh-60px)] dark:bg-[#030712]">
+        {navigation.state === "loading" ? <Loading1></Loading1> : <Outlet />}
       </div>
       <CustomFooter />
     </div>
